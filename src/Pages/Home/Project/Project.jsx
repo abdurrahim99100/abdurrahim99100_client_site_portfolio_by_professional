ffa319d7dada0b95b0d8684896a119ecfad56d7a
cornollc <abdurrahim99100@gmail.com>
2023-07-15 18:01:15 +0600

    project okay

--- src/Pages/Home/Project/Project.jsx
+++ src/Pages/Home/Project/Project.jsx
@@ -1,17 +1,68 @@
 import './project.css';
-import project1 from '../../../assets/project-photos/career-hub.png';
 const Project = () => {
     return (
-        <div id="project">
-            <div className="h-[100vh] flex flex-col justify-center items-center">
+        <div className='container mx-auto' id="project">
+            <div className="lg:h-[100vh] flex flex-col justify-center items-center">
                 <div className="flex flex-col gap-2 items-center font-semibold text-4xl">
                     <h3>Project</h3>
                     <progress className="progress bg-[#15E0B7] w-36 h-[5px]"></progress>
                 </div>
-                <div>
-                    <div className='box-container'>
-                        <div className='cover'>
-                            
+                <div className='mt-5 flex-none lg:flex gap-5 justify-between'>
+                    <div className='bg-[#23223b] p-5 rounded'>
+                        <div className='w-[340px] border-2 border-[#72968f] p-2 rounded'>
+                            <div className='box-container rounded flex justify-center'>
+                                <span className='text-[#15E0B7] uppercase font-bold mt-6'>Please hover enjoy Live preview</span>
+                            </div>
+                        </div>
+                        <div className='my-2'>
+                            <div className='flex gap-1 justify-start items-center'>
+                                <span>Live Link : </span>
+                                <a className='p-2 bg-gray-500 px-1 py-0 rounded' href="http://www.google.com">https://dance-club-bd.web.app</a>
+                            </div>
+                            <div className='flex justify-start items-center my-1'>
+                                <span className='mr-2'>Code Link :</span>
+                                <a className='mr-2 bg-[#15E0B7] px-2 rounded text-black' href="https://github.com/abdurrahim99100/creative_design_hub_client">Client Side</a>
+                                <a className='bg-[#15E0B7] px-2 rounded text-black' href="https://github.com/abdurrahim99100/creative_design_hub_server">Server Side</a>
+                            </div>
+                            <p className='w-[340px] text-sm font-light'>Description : Welcome to visitor, This is an educations project. I Finish this project on 03/06/2023. The technology I used in my project HTML5, CSS, TailwindCSS, javascript, Node.Js, Express.Js, JWT, MongoDb. The features that are in my project, three types of users can enter here, 1 normal user, 2 instructors, 3 admins. Normal user can enroll in any class, instructor can post any class, admin can control the total issue.</p>
+                        </div>
+                    </div>
+                    <div className='bg-[#23223b] p-5 rounded'>
+                        <div className='w-[340px] border-2 border-[#72968f] p-2 rounded'>
+                            <div className='box-container rounded flex justify-center'>
+                                <span className='text-[#15E0B7] uppercase font-bold mt-6'>Please hover enjoy Live preview</span>
+                            </div>
+                        </div>
+                        <div className='my-2'>
+                            <div className='flex gap-1 justify-start items-center'>
+                                <span>Live Link : </span>
+                                <a className='p-2 bg-gray-500 px-1 py-0 rounded' href="http://www.google.com">https://dance-club-bd.web.app</a>
+                            </div>
+                            <div className='flex justify-start items-center my-1'>
+                                <span className='mr-2'>Code Link :</span>
+                                <a className='mr-2 bg-[#15E0B7] px-2 rounded text-black' href="https://github.com/abdurrahim99100/creative_design_hub_client">Client Side</a>
+                                <a className='bg-[#15E0B7] px-2 rounded text-black' href="https://github.com/abdurrahim99100/creative_design_hub_server">Server Side</a>
+                            </div>
+                            <p className='w-[340px] text-sm font-light'>Description : Welcome to visitor, This is an educations project. I Finish this project on 03/06/2023. The technology I used in my project HTML5, CSS, TailwindCSS, javascript, Node.Js, Express.Js, JWT, MongoDb. The features that are in my project, three types of users can enter here, 1 normal user, 2 instructors, 3 admins. Normal user can enroll in any class, instructor can post any class, admin can control the total issue.</p>
+                        </div>
+                    </div>
+                    <div className='bg-[#23223b] p-5 rounded'>
+                        <div className='w-[340px] border-2 border-[#72968f] p-2 rounded'>
+                            <div className='box-container rounded flex justify-center'>
+                                <span className='text-[#15E0B7] uppercase font-bold mt-6'>Please hover enjoy Live preview</span>
+                            </div>
+                        </div>
+                        <div className='my-2'>
+                            <div className='flex gap-1 justify-start items-center'>
+                                <span>Live Link : </span>
+                                <a className='p-2 bg-gray-500 px-1 py-0 rounded' href="http://www.google.com">https://dance-club-bd.web.app</a>
+                            </div>
+                            <div className='flex justify-start items-center my-1'>
+                                <span className='mr-2'>Code Link :</span>
+                                <a className='mr-2 bg-[#15E0B7] px-2 rounded text-black' href="https://github.com/abdurrahim99100/creative_design_hub_client">Client Side</a>
+                                <a className='bg-[#15E0B7] px-2 rounded text-black' href="https://github.com/abdurrahim99100/creative_design_hub_server">Server Side</a>
+                            </div>
+                            <p className='w-[340px] text-sm font-light'>Description : Welcome to visitor, This is an educations project. I Finish this project on 03/06/2023. The technology I used in my project HTML5, CSS, TailwindCSS, javascript, Node.Js, Express.Js, JWT, MongoDb. The features that are in my project, three types of users can enter here, 1 normal user, 2 instructors, 3 admins. Normal user can enroll in any class, instructor can post any class, admin can control the total issue.</p>
                         </div>
                     </div>
                 </div>
